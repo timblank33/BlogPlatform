019cfb49dcb86ed09b6d4194362568f07e816f9b
Artyom Aksenov <timblank@gmail.com>
2023-12-06 13:40:00 +0300

    rework

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -28,7 +28,7 @@ function App() {
       currentPage(localStorage.setItem('pageNumber', 1));
     }
     dispatch(currentPage(+localStorage.getItem('pageNumber')));
-    dispatch(fetchList((+localStorage.getItem('pageNumber') - 1) * 5));
+    dispatch(fetchList((pageNumber - 1) * 5));
   }, [dispatch]);
 
   const articlesRender = (
